e1a586cdf7ef0bd67b56c10198925dda709bf6a5
Stefania Formato <sformato95@gmail.com>
2025-11-19 23:39:42 +0100

    Fix a bug of flip card

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const carte = document.querySelectorAll('.carta');
 
 let cartaGirata = false;
+let bloccaBoard = false;
 let primaCarta, secondaCarta;
 
 function giraCarta() {
+    if(bloccaBoard) return;
+    if(this === primaCarta) return;
     this.classList.add('flip');
 
     if(!cartaGirata){
@@ -32,15 +35,24 @@ function controllaCorrispondenza () {
 function disabilitaCarte () {
     primaCarta.removeEventListner('click', giraCarta);
     secondaCarta.removeEventListner('click', giraCarta);
+
+    resetBoard();
 }
 
 function rigiraCarte () {
+    bloccaBoard = true;
+
     setTimeout(() => {
         primaCarta.classList.remove('flip');
         secondaCarta.classList.remove('flip');
+        resetBoard();
     }, 1500);
 }
-    
 
+function resetBoard() {
+    [cartaGirata, bloccaBoard] = [false, false];
+    [primaCarta, secondaCarta] = [null, null];
+}
+    
 carte.forEach(carta => carta.addEventListener('click', giraCarta));
 
