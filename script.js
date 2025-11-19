f8a1cb5310dd7f814f6c20c62c88f96b2d107e9e
Stefania Formato <sformato95@gmail.com>
2025-11-19 23:27:51 +0100

    Add logistic of the card and modify style.css

--- script.js
+++ script.js
@@ -1,7 +1,46 @@
 const carte = document.querySelectorAll('.carta');
 
+let cartaGirata = false;
+let primaCarta, secondaCarta;
+
 function giraCarta() {
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+
+    if(!cartaGirata){
+        cartaGirata = true;
+        primaCarta = this;
+        return;
+    }
+
+    secondaCarta = this;
+    cartaGirata = false;
+
+    controllaCorrispondenza();
 }
 
+function controllaCorrispondenza () {
+    // if(primaCarta.dataset.forma === secondaCarta.dataset.forma) {
+    //     disabilitaCarte();
+    //     return;
+    // }
+
+    // rigiraCarte();
+    let corrisponde =  primaCarta.dataset.forma === secondaCarta.dataset.forma ? disabilitaCarte(): rigiraCarte();
+
+}
+
+function disabilitaCarte () {
+    primaCarta.removeEventListner('click', giraCarta);
+    secondaCarta.removeEventListner('click', giraCarta);
+}
+
+function rigiraCarte () {
+    setTimeout(() => {
+        primaCarta.classList.remove('flip');
+        secondaCarta.classList.remove('flip');
+    }, 1500);
+}
+    
+
 carte.forEach(carta => carta.addEventListener('click', giraCarta));
+
